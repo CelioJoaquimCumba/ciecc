8f6ebbda443ee80eb2417d92435d4681b8f9003a
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-09-30 12:56:14 +0200

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-black w-screen h-screen text-white text-7xl'>
+        Start
+      </div>
+  )
+}
+
+export default App
