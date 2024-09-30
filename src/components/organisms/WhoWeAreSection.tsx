dc16f688763fa585b61ad0d649416dc9d9096a40
CelioJoaquimCumba <onit.frontend1@lalgy.co.mz>
2024-09-30 14:27:18 +0200

    feat: who we are section

--- src/components/organisms/WhoWeAreSection.tsx
+++ src/components/organisms/WhoWeAreSection.tsx
@@ -1,7 +1,18 @@
+import BoyStudying from '@/assets/boy-studying.png'
 export default function WhoWeAreSection () {
     return (
-        <div>
-            <p>who we are section</p>
-        </div>
+        <section className="flex flex-col gap-8 p-16 bg-white">
+            <div className="flex flex-col gap-3">
+                <h3 className="text-4xl text-gray-600 font-bold">Quem somos?</h3>
+                <p className="text-lg max-w-2xl">
+                    Somos o Cantinho do Céu, uma comunidade educativa que valoriza a formação integral dos alunos, combinando ensino de qualidade com valores éticos e espirituais para formar cidadãos conscientes e transformadores.
+                </p>
+            </div>
+            <div className='grid grid-cols-3 grid-rows-1 gap-3'>
+                <img  src={BoyStudying} alt="boy"/>
+                <img src={BoyStudying} alt="boy" />
+                <img src={BoyStudying} alt="boy"/>
+            </div>
+        </section>
     )
 }
